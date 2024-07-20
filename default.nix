0fcd0b9ef75ee34ef5a61bbe5e3afee6c4acafff
Breeze256 <76651328+breeze256@users.noreply.github.com>
2024-07-20 17:03:02 +0800

    update: remove some comments

--- default.nix
+++ default.nix
@@ -1,11 +1,3 @@
-# This file describes your repository contents.
-# It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
-# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
-# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
-# commands such as:
-#     nix-build -A mypackage
-
 { pkgs ? import <nixpkgs> { } }:
 
 {
@@ -15,6 +7,5 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   gruvbox-material-gtk = pkgs.callPackage ./pkgs/gruvbox-material-gtk { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
